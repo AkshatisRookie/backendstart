39aecfbe553df6e4ec0fcc0705c34f94e91bbb6c
Akshat Seth <u23ee101@eed.svnit.ac.in>
2025-01-05 00:07:58 +0530

    comment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server:{
-     proxy:{
-      '/api': 'http://localhost:3000', //cors error hataya taaki /api ko apna manle
+     proxy:{ //used for error solving
+      '/api': 'http://localhost:3000', //cors error hataya taaki or api ko apna manle and limited hi file aa skti
      },
   },
   plugins: [react()],
