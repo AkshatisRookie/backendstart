bd4a8282e03f18918427b6761cbc150a7c1bef1b
Akshat Seth <u23ee101@eed.svnit.ac.in>
2025-01-07 23:20:06 +0530

    mongoose connection with backend

---
+++ backend/src/index.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+//adding IIFE(immediately invoked function expression)
+//global scope me extra info add nahi hoti
+//written in a particular way
+;(async()=>{
+    try{
+        
+    }catch(error){
+        console.log("Error",error)
+        throw error
+    }
+})()
