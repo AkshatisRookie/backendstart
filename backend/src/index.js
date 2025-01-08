d67a1a66b9853405c961dc6cb643e549368036d1
Akshat Seth <u23ee101@eed.svnit.ac.in>
2025-01-08 23:56:22 +0530

    basics database connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,24 @@
 import mongoose from "mongoose";
+import { database_namer } from "./constants";
+import express from "express"
+/*
 //adding IIFE(immediately invoked function expression)
 //global scope me extra info add nahi hoti
 //written in a particular way
+const app=express()
 ;(async()=>{
     try{
-        
+        await mongoose.connect(`${process.send.mongodb_url}`) //connected with database dont forget to add await
+        app.on("error" ,() =>{
+            console.log("ERR:",error);
+            throw error
+        })
+        app.listen(process.env.PORT,()=>{
+            console.log(`app is listening on port ${process.env.PORT} `);
+        })
     }catch(error){
         console.log("Error",error)
         throw error
     }
 })()
+*/
